b5cb67f8354d1733f529203578fc25f530a1e564
Tomás Mazzo <116767949+sirMazo@users.noreply.github.com>
2023-06-23 15:50:04 -0300

    Update navBar & responsive

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -8,24 +8,25 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 export class NavBarComponent implements OnInit {
   navbarVisible: boolean = true;
   contentFlex: string;
-  menuPosition: { top: string, right: string } = { top: '5px', right: '5px' };
 
 
   constructor(private renderer: Renderer2) {
     // Establecer porcentaje por defecto
-    this.contentFlex = '0 0 80%';
+    this.contentFlex = '0 0 90%';
   }
 
 
   toggleNavbar() {
     const sidebarElement = document.querySelector('.sidebar');
     const contentElement = document.querySelector('.main_content');
+
     if (sidebarElement && contentElement) {
       const sidebarHidden = sidebarElement.classList.contains('hidden');
+      
       if (sidebarHidden) {
         this.renderer.removeClass(sidebarElement, 'hidden');
         this.renderer.removeClass(contentElement, 'move-left');
-        this.contentFlex = '0 0 80%';
+        this.contentFlex = '0 0 90%';
       } else {
         this.renderer.addClass(sidebarElement, 'hidden');
         this.renderer.addClass(contentElement, 'move-left');
